43889d727925f30ee85e9a978859153ef951b106
bgoussama <o.bagy0805@uca.ac.ma>
2026-05-14 20:29:22 +0100

    feat: generate pipeline — Pipeline CI/CD pour FastAPI avec pytest, bandit, safety check, SonarQube, build

---
+++ terraform/main.tf
@@ -0,0 +1,32 @@
+provider 'aws' {
+    region = 'us-west-2'
+}
+
+resource 'aws_security_group' 'nextgen-sg' {
+    name        = 'nextgen-sg'
+    description = 'Deny all by default'
+    vpc_id      = 'vpc-12345678'
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = 'tcp'
+        cidr_blocks = ['0.0.0.0/0']
+    }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = '-1'
+        cidr_blocks = ['0.0.0.0/0']
+    }
+    tags = {
+        Name = 'nextgen-sg'
+    }
+}
+
+resource 'aws_instance' 'nextgen-ec2' {
+    ami           = 'ami-12345678'
+    instance_type = 't2.micro'
+    vpc_security_group_ids = [aws_security_group.nextgen-sg.id]
+    monitoring = true
+}
